b875b076e42fecc2a14091c937df7a9d25c4b5f1
Mario <minmarmario@gmail.com>
2026-03-20 13:07:54 +0100

    guardo para nuevo script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-const arreglos= []
-const lista_frutas=["pera","manzana","fresa"]
+const arreglo = [];
+const lista_frutas = ["pera", "manzana", "fresa"];
 
-console.log(arreglos)
-console.log(lista_frutas)
+console.log(arreglo);
+console.log(lista_frutas);
+
+arreglo[0] = "hola";
+arreglo[1] = 5;
+
+console.log("k");
+console.log(lista_frutas[2])
