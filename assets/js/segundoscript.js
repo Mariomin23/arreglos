b01ce32329eebaa1098108fc4d8ed56cdde2b61b
Mario <minmarmario@gmail.com>
2026-03-20 13:44:33 +0100

    explicacion

---
+++ assets/js/segundoscript.js
@@ -0,0 +1,9 @@
+const estudiantes = ["Carlos", "Priscila", "Jaume", "Elisa", "Mario"];
+
+console.log(estudiantes[4])
+
+// 1. Se coloca la variable de tipo constante, estudiantes
+// 2. Se enumera a los estudiantes contando que comenzamos por cero (0)
+// 3. Se localiza el array del propio nombre mediante console.log en la const indicada (En este caso, "estudiantes")
+// 4. Indicamos la posicion mediante: "console.log(estudiantes[4])" indicando dentro del array que posicion estamos buscando, que a su vez ese array esta en los corchetes de la variable "estudiantes"
+//5. Inspeccionamos consola y nos da "Mario" como valor. 
